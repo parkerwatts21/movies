0cd25f748fdb300002e5456b8b6403df4f5b817e
parkerwatts21 <parkerwatts21@gmail.com>
2024-02-14 00:04:19 -0700

    ASP FOR

---
+++ HobbiesWebApp/Models/Application.cs
@@ -0,0 +1,15 @@
+﻿namespace Mission07_Watts.Models
+{
+    public class Application
+    {
+        public int movieID { get; set; }
+        public string movieTitle { get; set; }
+        public string movieCategory { get; set; }
+        public int movieYear { get; set; }
+        public string movieDirector { get; set; }
+        public string movieRating { get; set; }
+        public bool movieEdited { get; set; }
+        public string movieLent{ get; set; }
+        public string movieNotes { get; set; }
+    }
+}
