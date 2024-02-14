8467af63f57c1157a69f7af933a4db9478d50aa9
parkerwatts21 <parkerwatts21@gmail.com>
2024-02-14 15:27:19 -0700

    Update

--- HobbiesWebApp/Models/Application.cs
+++ HobbiesWebApp/Models/Application.cs
@@ -7,15 +7,21 @@ namespace Mission07_Watts.Models
         [Key]
         [Required]
         public int movieID  { get; set; }
+        [Required]
         public string movieTitle { get; set; }
-        public string movieCategory { get; set; }
+        [Required]
+        public string movieCategoryOne { get; set; }
+        public string movieCategoryTwo { get; set; } = "";
+        [Required]
         public int movieYear { get; set; }
+        [Required]
         public string movieDirector { get; set; }
+        [Required]
         public string movieRating { get; set; }
-        public bool movieEdited { get; set; }
-        public string movieLent{ get; set; }
+        public bool? movieEdited { get; set; }
+        public string? movieLent{ get; set; }
 
         [MaxLength(25)]
-        public string movieNotes { get; set; }
+        public string? movieNotes { get; set; }
     }
 }
