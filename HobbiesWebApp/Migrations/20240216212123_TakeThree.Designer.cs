6a9b91644fcf8675ce8699bff87f52864b17e4f5
parkerwatts21 <parkerwatts21@gmail.com>
2024-02-16 14:27:59 -0700

    Last couple changes

--- HobbiesWebApp/Migrations/20240216212123_TakeThree.Designer.cs
+++ HobbiesWebApp/Migrations/20240216212123_TakeThree.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mission07_Watts.Models;
 
@@ -10,9 +11,11 @@ using Mission07_Watts.Models;
 namespace Mission07_Watts.Migrations
 {
     [DbContext(typeof(MovieApplicationContext))]
-    partial class MovieApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240216212123_TakeThree")]
+    partial class TakeThree
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
---
+++ HobbiesWebApp/Migrations/20240216212123_TakeThree.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission07_Watts.Migrations
+{
+    /// <inheritdoc />
+    public partial class TakeThree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "movieCategoryTwo",
+                table: "Applications",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "movieCategoryTwo",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
